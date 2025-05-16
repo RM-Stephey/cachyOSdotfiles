7946070956a1300d2627b1883c3c274c1d3d05ce
Stephen McWhorter <stephenmcwhorter@proton.me>
2025-05-16 02:54:02 -0400

    cc

--- scripts/update-widgets.sh
+++ scripts/update-widgets.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Update weather and calendar automatically
+trap "exit" SIGTERM
+
 while true; do
   # Update weather every 30 minutes
   ~/.config/hypr/scripts/show-weather.sh
-  
+
   # Update calendar daily
   ~/.config/hypr/scripts/show-calendar.sh
-  
+
   # Sleep for 30 minutes
   sleep 1800
 done
